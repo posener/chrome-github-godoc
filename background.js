52644dbdeadbfb884c2e437e39633a88e90369d8
Eyal Posener <posener@gmail.com>
2019-06-16 23:24:36 +0300

    Add current page doc

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onConnect.addListener(function(port) {
-	port.onMessage.addListener(function(message) {
-		console.log("Processing project:", message.project);
-		fetch("https://api.godoc.org/search?q=" + message.project + "/.*")
+	port.onMessage.addListener(function(view) {
+		console.log("Processing package:", view.package);
+		fetch("https://api.godoc.org/search?q=" + view.package + "/.*")
 			.then(function(response) {
 				if (response.status !== 200) {
 					console.log("Bad status code:", response.status);
--- content.js
+++ content.js
@@ -1,65 +1,95 @@
 
 function main() {
-	const project =  projectFromURL(document.location.href);
-	if (project == undefined) {
+	const view =  viewFromURL(document.location.href);
+	if (view == undefined) {
 		console.log("Invalid URL:", document.location.href);
 		return;
 	}
 
-	console.log("starting for project: ", project);
+	console.log("starting for view: ", view);
 	var port = chrome.runtime.connect({name: "fetch"});
 
 	// Register a listener to to process results returned from background script.
 	port.onMessage.addListener(function(results) {
+		if (results == undefined) {
+			console.log('No data for current path');
+			return;
+		}
 		console.log("got data: ", results);
 		const lookup = mapResults(results);
 
-		var table = document.getElementsByClassName("files")[0];
-		Array.from(table.getElementsByClassName("js-navigation-item")).forEach(function(row) {
-			if (!isDirectoryRow(row)) {
-				return;
-			}
+		updateHeader(view, lookup)
+		updateFilesList(view, lookup)
 
-			var name = row.getElementsByClassName("content")[0].getElementsByTagName("a")[0];
-			var message = row.getElementsByClassName("message")[0];
-			var synopsisBox = message.getElementsByTagName("a")[0];
-
-			var subPackage = subPackageFromPath(name.getAttribute("href"));
-			if (subPackage === undefined) {
-				log.console("skipping regex mismatched:", name);
-				return;
-			}
-			var pkg = project + "/" + subPackage;
-			var entry = lookup[pkg];
-			if (entry === undefined) {
-				return;
-			}
-			var synopsis = synopsisOf(entry);
-			console.log("Replacing", pkg, ":", synopsisBox.innerHTML, '->', synopsis);
-			synopsisBox.innerHTML = synopsis;
-			message.prepend(godocLink(entry));
-		});
 	});
 
-	// Send a message to background script to process the current project.
-	port.postMessage({project: project});
+	// Send a message to background script to process the current pkg.
+	port.postMessage(view);
 }
 
-const reProject = new RegExp('^https\:\/\/(github\.com\/[^/]+\/[^/]+)');
-
-function projectFromURL(url) {
-	var matches = reProject.exec(url);
-	if (matches === undefined || matches.length < 2) {
+function updateHeader(view, lookup) {
+	const entry = lookup[view.package]
+	if (entry == undefined || entry.synopsis == undefined) {
 		return;
 	}
-	return matches[1];
+	console.log('Update header with package synopsis.')
+	var header = document.getElementsByClassName("commit-tease")[0];
+	while (header.firstChild) {
+		header.removeChild(header.firstChild);
+	}
+	header.appendChild(godocText(entry))
+}
+
+function updateFilesList(view, lookup) {
+	var table = document.getElementsByClassName("files")[0];
+	Array.from(table.getElementsByClassName("js-navigation-item")).forEach(function(row) {
+		if (!isDirectoryRow(row)) {
+			return;
+		}
+
+		var name = row.getElementsByClassName("content")[0].getElementsByTagName("a")[0];
+		var message = row.getElementsByClassName("message")[0];
+		var synopsisBox = message.getElementsByTagName("a")[0];
+
+		var subPackage = subPackageFromPath(name.getAttribute("href"));
+		if (subPackage == undefined) {
+			log.console("skipping regex mismatched:", name);
+			return;
+		}
+		var pkg = view.project + "/" + subPackage;
+		var entry = lookup[pkg];
+		if (entry == undefined || entry.synopsis == undefined) {
+			return;
+		}
+		var synopsis = synopsisOf(entry);
+		console.log("Replacing", pkg, ":", synopsisBox.innerHTML, '->', synopsis);
+		synopsisBox.innerHTML = synopsis;
+		message.prepend(godocLink(entry));
+	});
+}
+
+const reGithubPath = new RegExp('^http[s]\:\/\/(github\.com\/[^/]+\/[^/]+)(\/tree\/[^/]+(\/.*))?');
+
+function viewFromURL(url) {
+	var matches = reGithubPath.exec(url);
+	if (matches == undefined || matches[1] == undefined) {
+		return;
+	}
+	var view = {
+		project: matches[1],
+		package: matches[1]
+	};
+	if (matches[3] != undefined) {
+		view.package = view.package + matches[3]
+	}
+	return view;
 }
 
 const rePath = new RegExp('^\/[^/]+\/[^/]+\/tree\/[^/]+\/(.*)');
 
 function subPackageFromPath(path) {
 	var matches = rePath.exec(path);
-	if (matches === undefined || matches.length < 2) {
+	if (matches == undefined || matches.length < 2) {
 		return;
 	}
 	return matches[1];
@@ -74,9 +104,6 @@ function mapResults(results) {
 }
 
 function synopsisOf(entry) {
-	if (entry === undefined) {
-		return '';
-	}
 	var re = new RegExp('Package '+entry.name+' ');
 	var synopsis = entry.synopsis.replace(re, '');
 	return synopsis.charAt(0).toUpperCase() + synopsis.slice(1);
@@ -91,6 +118,14 @@ function godocLink(entry) {
 	img.setAttribute("height", "16")
 	img.setAttribute("width", "16")
 	a.appendChild(img);
+	return a;
+}
+
+function godocText(entry) {
+	var a = document.createElement("a");
+	a.setAttribute("href", "https://godoc.org/" + entry.path);
+	a.setAttribute("target", "_blank");
+	a.innerHTML = synopsisOf(entry);
 	return a;
 }
 
