b7857ebb83a7caabb1a41f9eaee38a0b810c8f69
Eyal Posener <posener@gmail.com>
2019-06-16 13:23:41 +0300

    Fix godoc fetch query
    `...` is used for all dependent imports, `.*` is used for all imports with the same prefix - which is what needed.

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onConnect.addListener(function(port) {
 	port.onMessage.addListener(function(message) {
 		console.log("Processing project:", message.project);
-		fetch("https://api.godoc.org/search?q=" + message.project + "/...")
+		fetch("https://api.godoc.org/search?q=" + message.project + "/.*")
 			.then(function(response) {
 				if (response.status !== 200) {
 					console.log("Bad status code:", response.status);
